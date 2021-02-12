11e0a73e6dea9383ef7cbfae830498d02955d611
Gabe-The-Dog <57034567+Gabe-The-Dog@users.noreply.github.com>
2021-02-12 10:04:09 +0400

    js govno

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const buttMore = document.getElementById("port-butt-id");
+const buttMore1 = document.getElementById("post-butt-id");
+const ninja = document.getElementById("ninja");
+const ninja1 = document.getElementById("ninja1");
+
+buttMore.addEventListener("click", function () {
+    ninja.style.display = 'block';
+
+});
+
+buttMore1.addEventListener("click", function () {
+    ninja1.style.display = 'block';
+
+});
