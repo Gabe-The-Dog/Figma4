5e4adc5cad3fa50713c545b295da7c788f6ac2e7
Gabe-The-Dog <57034567+Gabe-The-Dog@users.noreply.github.com>
2021-02-12 19:48:29 +0400

    smol fix

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ const buttMore = document.getElementById("port-butt-id");
 const buttMore1 = document.getElementById("post-butt-id");
 const ninja = document.getElementById("ninja");
 const ninja1 = document.getElementById("ninja1");
+const email = document.getElementById("email");
 
 buttMore.addEventListener("click", function () {
     ninja.style.display = 'block';
-
+    buttMore.style.display = 'none';
+    email.style.margin = "470px 102px 0";
 });
 
 buttMore1.addEventListener("click", function () {
     ninja1.style.display = 'block';
-
+    buttMore1.style.display = 'none';
 });
